09104c53f449e0b74f32e45d1b8161a089977309
SHRADDHAPHALE <sphale28@gmail.com>
2021-10-08 23:35:05 +0530

    First commit

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def delete():
 
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
